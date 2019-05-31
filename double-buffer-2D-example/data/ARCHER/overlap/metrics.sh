72ee1aca77fb0b8624afb50130daa93807d57890
Luis Cebamanos <luiceur@gmail.com>
2019-05-31 16:00:57 +0100

    Added ARCHER data

---
+++ double-buffer-2D-example/data/ARCHER/overlap/metrics.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+FILES=*_out
+
+##Get all required data. The output format is as follow
+# LatticeSize NumProc SampleNum TotalTime TimeperLoop TimeHaloExchange
+function getData(){
+    
+    for f in ${FILES}
+    do
+	#files are in this fashion LatticeSize_NumProc_SampleNum_API
+	echo  $f | awk -F'[_]' ' {printf "%s  %s ", $1, $2}' 
+#extract Total time and Lattice halo exchange time
+	awk '/Loop/ {printf "%1.4f ",$4}; /Computation/ {printf "%1.4f ", $4} ;/Communication/ {printf "%1.4f\n",$4}; ' $f
+    done
+    
+
+}
+
+### Start here ###
+
+getData |  sort -n -k 2
