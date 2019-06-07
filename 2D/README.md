024056cf5c2aec2b64f36e1287cdf92287c4361a
Luis Cebamanos <luiceur@gmail.com>
2019-06-07 16:28:29 +0100

    deleting directories

--- 2D/README.md
+++
@@ -1,2 +0,0 @@
-# WP2-halo-bench
-Double buffering halo exchange using MPI persistent point to point
--- overlap-2D-example/compute.h
+++
@@ -1,6 +0,0 @@
-
-void compute_edge_A();
-void compute_edge_B();
-void compute_middle_A();
-void compute_middle_B();
-
